3302daaecaf5e3047fca59269dd28ad6ccf64bfe
MPA-SE <priscillamomodu@gmail.com>
2022-12-13 17:01:32 -0800

    learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * times_table - Prints the nine times table
+ */
+
+void times_table(void)
+{
+	int x, mul, pro;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar('0');
+
+		for (mul = 1; mul <= 9; mul++)
+		{
+			_putchar('+');
+			_putchar(' ');
+
+			pro = x + mul;
+
+			if (pro <= 9)
+				_putchar(' ');
+			else
+				_putchar((pro / 10) + '0');
+
+			_putchar((pro % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
