1d738199801f1098fdde2b90a061cb68d2bc76ee
MPA-SE <priscillamomodu@gmail.com>
2022-12-13 17:42:13 -0800

    a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @y: the first integer
+ * @z: the second integer
+ * Return: The addition of both integers
+ */
+
+int add(int y, int z)
+{
+	return (y + z);
+}
