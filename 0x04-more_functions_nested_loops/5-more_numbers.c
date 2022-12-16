462619db65f78612cab39001acc08dd61e1a3e3d
MPA-SE <priscillamomodu@gmail.com>
2022-12-15 17:20:55 -0800

    Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0 to 14 ten times
+ * Return: 0
+ */
+
+void more_numbers(void)
+{
+	int a;
+	int p;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (p = 0; p < 15; p++)
+			_putchar(p + '0');
+	}
+	_putchar('\n');
+}
