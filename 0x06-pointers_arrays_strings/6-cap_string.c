7b0b16aad528c23bfec717e3580a1839eeec9ffd
MPA-SE <priscillamomodu@gmail.com>
2022-12-22 12:54:06 -0800

    capitalize all words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @stg: the string
+ * Return: 0
+ */
+
+char *cap_string(char *stg)
+{
+	int ind = 0;
+
+	while (stg[ind])
+	{
+		while (!(stg[ind] >= 'a' && stg[ind] <= 'z'))
+			ind++;
+
+				if (stg[ind - 1] == ' ' ||
+					stg[ind - 1] == 't' ||
+					stg[ind - 1] == '\n' ||
+					stg[ind - 1] == ',' ||
+					stg[ind - 1] == ';' ||
+					stg[ind - 1] == '.' ||
+					stg[ind - 1] == '!' ||
+					stg[ind - 1] == '?' ||
+					stg[ind - 1] == '"' ||
+					stg[ind - 1] == '(' ||
+					stg[ind - 1] == ')' ||
+					stg[ind - 1] == '{' ||
+					stg[ind - 1] == '}' ||
+					ind == 0)
+				stg[ind] -= 32;
+
+				ind++;
+	}
+	return (stg);
+}
