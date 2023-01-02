7a98e485eb1e393e27cfd41f0685961d67a12fc8
MPA-SE <priscillamomodu@gmail.com>
2023-01-02 12:27:14 -0800

    print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int h1, h2;
+
+	for (h1 = 0; a[h2][7]; h1++)
+	{
+		for (h2 = 0; h2 < 8; h2++)
+			_putchar(a[h1][h2]);
+
+		_putchar('\n');
+	}
+}
