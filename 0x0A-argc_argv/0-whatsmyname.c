14a07e253282871c47df7f28c49909dea51b0030
MPA-SE <priscillamomodu@gmail.com>
2023-01-07 03:56:27 -0800

    print its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
@@ -9,6 +10,9 @@
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", *argv);
+	(void) argc;
+
+	printf("%s\n", argv[0]);
+
 	return (0);
 }
