5d5a9cf5e4c13d8633bfaeffea0d01209ece02fe
MPA-SE <priscillamomodu@gmail.com>
2023-01-18 14:02:30 -0800

    whatz my name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -5,13 +5,11 @@
  * print_name - prints a name
  * @name: string
  * @f: pointer to function
- * Return: nothing
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
+	if (f != NULL)
 
 	f(name);
 }
